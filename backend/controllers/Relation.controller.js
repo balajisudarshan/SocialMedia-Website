edd0e7e9e7e72547eeb4dc2ba7ee8b0d6470f5fb
Kondreddy Balaji Sudarshan Reddy <kondreddy2007@gmail.com>
2026-02-25 20:55:10 +0530

    completed remove connection, mnage connection and get my connected people

--- backend/controllers/Relation.controller.js
+++ backend/controllers/Relation.controller.js
@@ -39,17 +39,106 @@ const sendRequest = async (req, res) => {
         console.log(err)
     }
 }
-const viewRequest = async(req,res)=>{
-    try{
+const viewRequest = async (req, res) => {
+    try {
         const userId = req.user
         const requests = await Relation.find({
-            receiver:userId,
-            status:"pending"
-        }).populate("sender","userName bio skills")
-        res.status(200).json({requests})
-    }catch(error){
+            receiver: userId,
+            status: "pending"
+        }).populate("sender", "userName bio skills")
+        res.status(200).json({ requests })
+    } catch (error) {
         console.error(error)
     }
 }
 
-module.exports = {sendRequest,viewRequest}
+const manageConnection = async (req, res) => {
+    try {
+        const userId = req.user
+        const requestId = req.params.id
+        const allowedConnectionType = [
+            'accept',
+            'reject'
+        ]
+        const connectionType = req.params.type
+        
+        if (!allowedConnectionType.includes(connectionType)) {
+            return res.status(400).json({ message: "Invalid Type" })
+        }
+        const request = await Relation.findOne({
+            _id:requestId,
+            receiver:userId,
+            status:"pending"
+        })
+
+        if (!request) {
+            return res.status(404).json({ message: "Connection not found" })
+        }
+
+        if (connectionType === 'reject') {
+            request.status = 'rejected'
+            await request.save()
+            return res.status(200).json({ message: "Request rejected" })
+        }
+
+        request.status = 'accepted';
+        await request.save()
+
+        const senderId = request.sender;
+        const receiverId = request.receiver
+
+        await User.findByIdAndUpdate(receiverId, {
+            $addToSet: { following: senderId, followers: senderId }
+        })
+
+        await User.findByIdAndUpdate(senderId, {
+            $addToSet: { following: receiverId, followers: receiverId }
+        })
+
+        res.status(200).json({ message: "Connection accepted" })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+const removeConnection = async (req, res) => {
+    try {
+        const user = req.user
+        const connectedId = req.params.id
+        const currentUser = await User.findById(user)
+        const isConnected = currentUser.following.includes(connectedId);
+
+        
+        if(!isConnected){
+            return res.status(404).json({message:"Connection not found"})
+        }
+        await User.findByIdAndUpdate(user,{
+            $pull:{following:connectedId,followers:connectedId}
+        })
+        await User.findByIdAndUpdate(connectedId,{
+            $pull:{following:user,followers:user}
+        })
+        await Relation.deleteMany({
+            $or:[
+                {sender:user,receiver:connectedId},
+                {sender:connectedId,receiver:user}
+            ]
+        })
+        res.status(200).json({message:"Connection Removed"});
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const getMyConnections = async(req,res)=>{
+    try{
+        const userId = req.user
+
+        const user = await User.findById(userId).populate("following","userName,bio,skills avatar")
+
+        res.status(200).json(user.follwing)
+    }catch(err){
+        console.log(err)
+    }
+}
+module.exports = { sendRequest, viewRequest, manageConnection,removeConnection,getMyConnections }
--- backend/routes/childRoutes/Relation.route.js
+++ backend/routes/childRoutes/Relation.route.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const authMiddleware = require('../../middleware/auth')
-const {sendRequest,viewRequest} = require('../../controllers/Relation.controller')
+const {sendRequest,viewRequest,removeConnection,manageConnection,getMyConnections} = require('../../controllers/Relation.controller')
 
 
 router.post('/send/:id',authMiddleware,sendRequest);
+router.post('/remove/:id',authMiddleware,removeConnection)
+router.post('/:type/:id',authMiddleware,manageConnection)
 router.get('/getMyRequests',authMiddleware,viewRequest)
+router.get('/my',authMiddleware,getMyConnections)
 module.exports = router
