f7e44a9f47fe560d99c462705bac5cf7dc4feaf5
Kondreddy Balaji Sudarshan Reddy <kondreddy2007@gmail.com>
2026-03-02 20:18:21 +0530

    refacored the usercard

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -17,11 +17,12 @@ import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { useAuth } from "@/context/AuthContext"
 import FeedSkeleton from "@/components/FeedSkeleton"
-import { Linkedin, Github, MessageCircle } from "lucide-react"
+import UsersCard from "@/components/UsersCard"
 
 const Page = () => {
   const [projects, setProjects] = useState([])
   const [loading, isLoading] = useState(true)
+  const [usersLoading, setUsersLoading] = useState(true)
   const [users, setUsers] = useState([])
   const user = useAuth()
   useEffect(() => {
@@ -39,11 +40,14 @@ const Page = () => {
 
     const fetchUsers = async () => {
       try {
+        setUsersLoading(true)
         const res = await api.get('/user?limit=6')
         console.log(res.data.users)
         setUsers(res.data.users)
       } catch (error) {
         console.log(error)
+      } finally {
+        setUsersLoading(false)
       }
     }
     fetchUsers()
@@ -81,60 +85,12 @@ const Page = () => {
           Explore People
         </h1>
 
-        <div className="flex gap-4 justify-center">
+        <div className="flex flex-wrap gap-4 justify-center">
           {users.map((user) => {
             const visibleSkills = user.skills.slice(0, 2)
             const remainingCount = user.skills.length - 2
             return (
-              <div key={user._id} className="w-full md:w-[45%] lg:w-[30%] p-6 bg-card border rounded shadow-sm hover:shadow-lg transition-all duration-300 flex flex-col justify-between">
-                <div className="flex items-start gap-4 ">
-                  <Avatar className='h-14 w-14'>
-                    <AvatarImage src={user.avatar} />
-                    <AvatarFallback className='text-lg font-semibold'>{user.firstName.charAt(0).toUpperCase()}</AvatarFallback>
-                  </Avatar>
-                  <div className="flex flex-col">
-                    <h1 className="font-semibold text-lg leading-tight">{user.firstName} {user.lastName}</h1>
-                    <h2 className="text-sm text-muted-foreground">@{user.userName}</h2>
-                    <h2 className="text-xs text-muted-foreground mt-1">Connctions : {user.following.length}</h2>
-                  </div>
-
-                </div>
-
-
-                <div className="flex flex-wrap gap-3 mt-4">
-                  {visibleSkills.map((skill) => {
-                    return (
-                      <Badge key={skill}>{skill}</Badge>
-                    )
-                  })}
-                  {remainingCount > 0 && (
-                    <Badge>+ {(remainingCount)}</Badge>
-                  )}
-                </div>
-                <div className="flex justify-between mt-3">
-                  <div className="flex gap-2 mt-2">
-
-                    {user.contactLinks.discord && (
-                      <Button size="sm" variant="outline" className="flex items-center gap-2">
-                        <MessageCircle size={16} />
-                      </Button>
-                    )}
-
-                    {user.contactLinks.linkedIn && (
-                      <Button size="sm" variant="outline" className="flex items-center gap-2 hover:bg-blue-600">
-                        <Linkedin size={16} />
-                      </Button>
-                    )}
-
-                    {user.contactLinks.github && (
-                      <Button size="sm" variant="outline" className="flex items-center gap-2">
-                        <Github size={16} />
-                      </Button>
-                    )}
-                  </div>
-                  <Button>Connect</Button>
-                </div>
-              </div>
+              <UsersCard key={user._id} user={user} visibleSkills={visibleSkills} remainingCount={remainingCount} />
             )
           })}
         </div>
---
+++ frontend/src/components/UsersCard.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Avatar,AvatarImage,AvatarFallback } from './ui/avatar'
+import { Badge } from './ui/badge'
+import { Button } from './ui/button'
+import { Linkedin, Github, MessageCircle, GitBranch } from "lucide-react"
+
+const UsersCard = ({user,visibleSkills,remainingCount}) => {
+    return (
+        <div  className="w-full md:w-[45%] lg:w-[30%] p-6 bg-card border rounded-xl shadow-sm hover:shadow-lg transition-all duration-300 flex flex-col justify-between">
+            <div className="flex items-start gap-4 ">
+                <Avatar className='h-14 w-14'>
+                    <AvatarImage src={user.avatar} />
+                    <AvatarFallback className='text-lg font-semibold'>{user.firstName.charAt(0).toUpperCase()}</AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                    <h1 className="font-semibold text-lg leading-tight">{user.firstName} {user.lastName}</h1>
+                    <h2 className="text-sm text-muted-foreground">@{user.userName}</h2>
+                    <h2 className="text-xs text-muted-foreground mt-1">Connctions : {user.following.length}</h2>
+                </div>
+
+            </div>
+
+
+            <div className="flex flex-wrap gap-3 mt-4">
+                {visibleSkills.map((skill) => {
+                    return (
+                        <Badge key={skill}>{skill}</Badge>
+                    )
+                })}
+                {remainingCount > 0 && (
+                    <Badge>+ {(remainingCount)}</Badge>
+                )}
+            </div>
+            <div className="flex justify-between mt-3">
+                <div className="flex gap-2 mt-2">
+
+                    {user.contactLinks.discord && (
+                        <Button size="sm" variant="outline" className="flex items-center gap-2">
+                            <MessageCircle size={16} />
+                        </Button>
+                    )}
+
+                    {user.contactLinks.linkedIn && (
+                        <Button size="sm" variant="outline" className="flex items-center gap-2 hover:bg-blue-600">
+                            <Linkedin size={16} />
+                        </Button>
+                    )}
+
+                    {user.contactLinks.github && (
+                        <Button size="sm" variant="outline" className="flex items-center gap-2">
+                            <Github size={16} />
+                        </Button>
+                    )}
+                </div>
+                <Button>Connect</Button>
+            </div>
+        </div>
+    )
+}
+
+export default UsersCard
